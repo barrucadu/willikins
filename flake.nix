d595701bf8ad1827fde6505699b260b2c3474d68
Michael Walker <mike@barrucadu.co.uk>
2025-04-15 18:21:55 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      formatter.${system} = pkgs.nixpkgs-fmt;
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs.haskellPackages; [
+          cabal-install
+          ghc
+          hlint
+        ];
+      };
+
+      packages.${system}.default = pkgs.haskellPackages.developPackage {
+        root = ./.;
+      };
+
+      apps.${system}.lint = {
+        type = "app";
+        program = toString (pkgs.writeShellScript "lint.sh" "${pkgs.haskellPackages.hlint}/bin/hlint .");
+      };
+    };
+}
